b43effe9da380f01bbc192f833a8ad135cef81c0
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-02 02:51:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,6 @@ end
 def using_delete(array,string)
   array.delete(string)
 end
-def using_delete_at(array,index)
-  array.delete(2)
+def using_delete_at(array,integer)
+  array.delete_at(integer)
 end
